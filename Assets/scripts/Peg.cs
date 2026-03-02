902aea07e04201670699a5dd58b4aa3be3295757
Kenny031782 <ken051782@gmail.com>
2026-03-02 00:50:17 -0800

    added peg behavior

---
+++ Assets/scripts/Peg.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Peg : MonoBehaviour
+{
+    public List<Sprite> Sprites;
+
+    private int currentSpriteNumber = 0;
+    private SpriteRenderer spriteRenderer;
+
+    public void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = Sprites[currentSpriteNumber];
+    }
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            OnBallHit();
+        }
+    }
+
+    private void OnBallHit()
+    {
+        // if no more sprites show
+            // destroy
+        if (NoMoreSprites())
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            ShowNextSprite();
+        }
+        // else
+            // show the next sprite
+    }
+
+    private void ShowNextSprite()
+    {
+        currentSpriteNumber++;
+        spriteRenderer.sprite = Sprites[currentSpriteNumber];
+    }
+
+    private bool NoMoreSprites()
+    {
+        if (currentSpriteNumber == 2)
+            return true;
+        return false;
+        
+    }
+}
