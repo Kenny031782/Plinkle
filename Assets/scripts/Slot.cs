b2244bb7ae574b75c6674335cb052e6fa5636a40
Kenny031782 <ken051782@gmail.com>
2026-03-02 18:34:12 -0800

    made firework effect when player scores

--- Assets/scripts/Slot.cs
+++ Assets/scripts/Slot.cs
@@ -4,6 +4,8 @@ public class Slots : MonoBehaviour
 {
     public int Points = 10;
     public UI Ui;
+    public GameObject FireworksParticlePrefab;
+    public Sounds Sounds;
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Ball"))
@@ -12,9 +14,29 @@ public class Slots : MonoBehaviour
         }
     }
 
-    private void OnBallEntered(Collider2D other)
+    private void OnBallEntered(Collider2D ball)
     {
         ScorePoint();
+        SpawnParticles(ball);
+        PlayFireworksSounds();
+    }
+
+    private void PlayFireworksSounds()
+    {
+        Sounds.PlayFireworksSound();
+    }
+
+    private void SpawnParticles(Collider2D ball)
+    {
+        Vector3 particlePosition = GetParticlePosition(ball);
+        Instantiate(FireworksParticlePrefab, transform.position, Quaternion.identity);
+    }
+
+    private Vector3 GetParticlePosition(Collider2D ball)
+    {
+        Vector3 ballPosition = ball.transform.position;
+        ballPosition.y += 0.25f;
+        return ballPosition;
     }
 
     private void ScorePoint()
